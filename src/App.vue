fabc8e67af008df029566a82ce6365047cd2af4f
icecodder <git@icecodder.me>
2021-10-02 19:57:47 +0200

    init vue3 with parcel2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen space-y-3">
+    <p class="text-5xl font-light">Welcome to <span class="font-extrabold text-green-500 underline">{{ website }}</span>!</p>
+    <p class="text-xl font-thin">🚧 This website is under construction 🚧</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      website: "icecodder.me"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-family: "Rubik", "Poppins", "sans-serif";
+}
+</style>
