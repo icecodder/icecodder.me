5bf271d3e6ac2656fc488c486d0f5e241809d81c
icecodder <git@icecodder.me>
2021-10-03 12:25:51 +0200

    add buttonrow

---
+++ src/components/ButtonRow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-wrap flex-row space-x-6 pt-3">
+    <Button class="flex-1 bg-button-email" href="mailto:root@icecodder.me" name="Email" />
+    <Button class="flex-1 bg-button-twitter" href="https://twitter.com/maxcharrier_" name="Twitter" />
+    <Button class="flex-1 bg-button-github" href="https://github.com/icecodder" name="GitHub" />
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  components: {
+    Button
+  }
+}
+</script>
