2ca316e55d74e33761dea637f9c3bf5605dd0a7c
icecodder <git@icecodder.me>
2021-10-03 17:15:16 +0200

    setup navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="flex-none flex flex-row h-12 mx-6 text-white">
+    <div class="flex flex-row h-full w-4/5 items-center">
+      <a class="text-2xl" href="/">icecodder.me</a>
+    </div>
+    <div class="flex flex-row-reverse h-full w-1/5 items-center">
+      <img class="h-6 ml-6" src="../assets/flags/fr_FR.png" alt="FR" />
+      <a class="text-lg" href="#about">À propos</a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+a {
+  font-family: "Rubik", "Poppins", "sans-serif";
+}
+</style>
