97de16b5841bfcf9e137d0689c5178bc5c6fceae
icecodder <git@icecodder.me>
2021-10-03 12:19:01 +0200

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="px-6 py-2 font-black text-white shadow-2xl" :href="href">{{ name }}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    href: String
+  }
+}
+</script>
+
+<style scoped>
+a {
+  font-family: "Rubik", "Poppins", "sans-serif";
+}
+</style>
+
